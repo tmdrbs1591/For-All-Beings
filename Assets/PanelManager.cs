0c398fde87bfe8e39a71988760b45e8247f7845c
tmdrbs1591 <sdh230211@sdh.hs.kr>
2024-09-07 15:25:34 +0900

    UI 수정

--- Assets/PanelManager.cs
+++ Assets/PanelManager.cs
@@ -20,16 +20,27 @@ public class PanelManager : MonoBehaviour
     public Button menuButton;      // Menu 버튼
     public Button characterButton; // Character 버튼
     public Button settingButton;   // Setting 버튼
-    public Button shopButton;   // Setting 버튼
+    public Button shopButton;      // Shop 버튼
 
     public Animator menuAnimator;      // Menu 패널의 Animator
     public Animator characterAnimator; // Character 패널의 Animator
     public Animator settingAnimator;   // Setting 패널의 Animator
-    public Animator shopButtonAnimator;   // Setting 패널의 Animator
+    public Animator shopButtonAnimator;   // Shop 패널의 Animator
+
+    private Dictionary<CurrentPanel, Animator> panelAnimators;  // 패널과 애니메이터 매핑
 
     void Awake()
     {
         instance = this;
+
+        // 패널과 애니메이터를 Dictionary에 등록
+        panelAnimators = new Dictionary<CurrentPanel, Animator>()
+        {
+            { CurrentPanel.Menu, menuAnimator },
+            { CurrentPanel.Character, characterAnimator },
+            { CurrentPanel.Setting, settingAnimator },
+            { CurrentPanel.Shop, shopButtonAnimator }
+        };
 
         // 버튼 클릭 시 각각 다른 패널로 전환
         menuButton.onClick.AddListener(() => PanelSetting(CurrentPanel.Menu));
@@ -37,39 +48,19 @@ public class PanelManager : MonoBehaviour
         settingButton.onClick.AddListener(() => PanelSetting(CurrentPanel.Setting));
         shopButton.onClick.AddListener(() => PanelSetting(CurrentPanel.Shop));
     }
+
     private void Update()
     {
-      switch(currentPanel)
+        UpdateAnimatorStates();
+    }
+
+    // 현재 패널에 맞는 애니메이터 상태만 활성화하는 함수
+    private void UpdateAnimatorStates()
+    {
+        foreach (var entry in panelAnimators)
         {
-            case CurrentPanel.Menu:
-                menuAnimator.SetBool("isSelect", true);
-                characterAnimator.SetBool("isSelect", false);
-                settingAnimator.SetBool("isSelect", false);
-                shopButtonAnimator.SetBool("isSelect", false);
-
-
-                break;
-            case CurrentPanel.Character:
-                characterAnimator.SetBool("isSelect", true);
-                menuAnimator.SetBool("isSelect", false);
-                settingAnimator.SetBool("isSelect", false);
-                shopButtonAnimator.SetBool("isSelect", false);
-
-                break;
-            case CurrentPanel.Setting:
-                settingAnimator.SetBool("isSelect", true);
-                menuAnimator.SetBool("isSelect", false);
-                characterAnimator.SetBool("isSelect", false);
-                shopButtonAnimator.SetBool("isSelect", false);
-
-                break;
-            case CurrentPanel.Shop:
-                shopButtonAnimator.SetBool("isSelect", true);
-                settingAnimator.SetBool("isSelect", false);
-                menuAnimator.SetBool("isSelect", false);
-                characterAnimator.SetBool("isSelect", false);
-
-                break;
+            // 현재 패널에 해당하는 애니메이터만 'isSelect'를 true로 설정, 나머지는 false
+            entry.Value.SetBool("isSelect", entry.Key == currentPanel);
         }
     }
 
@@ -77,6 +68,5 @@ public class PanelManager : MonoBehaviour
     {
         currentPanel = panel;
         Debug.Log("Current Panel: " + currentPanel.ToString());
-        // 여기에 각 패널에 맞는 로직 추가
     }
 }
