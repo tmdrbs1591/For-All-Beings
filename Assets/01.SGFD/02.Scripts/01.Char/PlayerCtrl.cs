d2361412f35198b20892f77b7ad0b4ce6f18fb88
tmdrbs1591 <sdh230211@sdh.hs.kr>
2024-09-19 08:50:33 +0900

    수정 | 동기ㅣ화 수정

--- Assets/01.SGFD/02.Scripts/01.Char/PlayerCtrl.cs
+++ Assets/01.SGFD/02.Scripts/01.Char/PlayerCtrl.cs
@@ -352,19 +352,23 @@ public class PlayerCtrl : MonoBehaviourPunCallbacks, IPunObservable
         // 동기화할 데이터가 있을 경우 여기에 작성합니다.
         if (stream.IsWriting)
         {
-            // 데이터를 다른 클라이언트에게 보냅니다.
-            stream.SendNext(playerStats.attackPower);
-            stream.SendNext(transform.position);
-            stream.SendNext(transform.rotation);
-            stream.SendNext(playerStats.curHp);
+            if (Vector3.Distance(transform.position, networkPosition) > 0.1f || Quaternion.Angle(transform.rotation, networkRotation) > 1f)
+            {
+                // 데이터를 다른 클라이언트에게 보냅니다.
+                stream.SendNext(playerStats.attackPower);
+                stream.SendNext(transform.position);
+                stream.SendNext(transform.rotation);
+                stream.SendNext(playerStats.curHp);
+            }
 
         }
         else
         {
-            // 데이터를 다른 클라이언트로부터 수신합니다.
-            playerStats.attackPower = (float)stream.ReceiveNext();
-            transform.position = (Vector3)stream.ReceiveNext();
-            transform.rotation = (Quaternion)stream.ReceiveNext();
+            networkPosition = (Vector3)stream.ReceiveNext();
+            networkRotation = (Quaternion)stream.ReceiveNext();
+
+        // 데이터를 다른 클라이언트로부터 수신합니다.
+           playerStats.attackPower = (float)stream.ReceiveNext();
             playerStats.curHp = (float)stream.ReceiveNext();
         }
     }
